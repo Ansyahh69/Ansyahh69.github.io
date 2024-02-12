225ffa1f268867699cb289db24fea800771a32b4
Ansyahh69 <159676409+Ansyahh69@users.noreply.github.com>
2024-02-12 14:20:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+document.getElementById('startButton').addEventListener('click', function() {
+    window.location.href = 'halaman2.html'; // Ganti 'halaman2.html' dengan URL halaman kedua Anda
+});
